595d3931e026c7f860fa4f6fe5d7fcdb81e35564
Anuj Datar <anuj.datar@gmail.com>
2023-07-30 17:07:49 +0530

    removed setup logic, added get_ip functions, better and concise logging

--- scripts/ddns-update.sh
+++ scripts/ddns-update.sh
@@ -1,97 +1,65 @@
 #!/bin/sh
 
-print_breaker() {
-    echo "-----------------------------------------------"
+. /config.sh
+
+# #####################################################################
+# functions to get public ip
+get_ip4() {
+  CURRENT_IP=$(curl -s https://ipv4.icanhazip.com/ || curl -s https://api.ipify.org)
+  if [ -z $CURRENT_IP ]; then
+    dig_ip=$(dig txt ch +short whoami.cloudflare @1.1.1.1)
+    if [ "$?" = 0 ]; then
+      CURRENT_IP=$(echo $dig_ip | tr -d '"')
+    else
+      exit 1
+    fi
+  fi
+  echo $CURRENT_IP
 }
 
-echo "Container spun up at:  [$(date)]"
-print_breaker
+get_ip6() {
+  CURRENT_IP=$(curl -s https://ipv6.icanhazip.com/ || curl -s https://api6.ipify.org)
+  if [ -z $CURRENT_IP ]; then
+    dig_ip=$(dig txt ch +short whoami.cloudflare @2606:4700:4700::1111)
+    if [ "$?" = 0 ]; then
+      CURRENT_IP=$(echo $dig_ip | tr -d '"')
+    else
+      exit 1
+    fi
+  fi
+  echo $CURRENT_IP
+}
 # #####################################################################
-# Step 1: Check auth secrets and env variables
-# 1. API key
-echo "Performing basic container parameter checks..."
-if [ -f "$API_KEY_FILE" ]; then
-  API_KEY=$(cat "$API_KEY_FILE")
-fi
-if [ -z "$API_KEY" ]; then
-  echo "Please enter valid API_KEY env variable or add /secrets/api_key file"
-  exit 1
-fi
-echo "API Key  ---  OK"
-
-# 2. Zone
-if [ -f "$ZONE_FILE" ]; then
-  ZONE=$(cat "$ZONE_FILE")
-fi
-if [ -z "$ZONE" ]; then
-  echo "Please enter valid ZONE env variable or add /secrets/zone file"
-  exit 1
-fi
-echo "Zone: $ZONE  ---  OK"
-print_breaker
-
-# 3. Record Type
+# Step 1: Get current public IP address
 if [ "$RECORD_TYPE" == "A" ]; then
-    echo "Record type to be updated: A (IPv4)"
+	CURRENT_IP=$(get_ip4)
+	PROTO="ipv4"
 elif [ "$RECORD_TYPE" == "AAAA" ]; then
-    echo "Record type to be updated: AAAA (IPv6)"
-else
-    RECORD_TYPE="A"
-    echo "Unknown record type, assuming A-record (IPv4)"
-fi
-
-# #####################################################################
-# Step 2: Get current public IP address
-echo fetching record type $RECORD_TYPE
-
-if [ "$RECORD_TYPE" == "A" ]; then
-	CURRENT_IP=$(curl -s https://api.ipify.org || curl -s https://ipv4.icanhazip.com/)
-
-	# check cloudflare's dns server if above method doesn't work
-	if [ -z $CURRENT_IP ]; then
-		echo using cloudflare whoami to find ip
-    CURRENT_IP=$(dig txt ch +short whoami.cloudflare @1.1.1.1 | tr -d '"')
-	fi
-elif [ "$RECORD_TYPE" == "AAAA" ]; then
-	CURRENT_IP=$(curl -s https://api6.ipify.org || curl -s https://ipv6.icanhazip.com/)
-
-	# check cloudflare's dns server if above method doesn't work
-	if [ -z $CURRENT_IP ]; then
-		echo using cloudflare whoami to find ip
-    CURRENT_IP=$(dig txt ch +short whoami.cloudflare @2606:4700:4700::1111 | tr -d '"')
-	fi
+	CURRENT_IP=$(get_ip6)
+	PROTO="ipv6"
 fi
 
 if [ -z $CURRENT_IP ]; then
-    echo "No public IP found: check internet connection or network settings"
-    exit 1
+	echo "[$(date)]: Public IP not found, check internet connection"
+	exit 1
 fi
-echo "Current time: [$(date)]"
-echo "Current Public IP: $CURRENT_IP"
-
 # #####################################################################
-# Step 3: Update ddns
-# check registered ip against current public ip
+# Step 2: Check against old IP
 OLD_IP=$(cat /old_record_ip)
 echo "Stored IP address $OLD_IP"
 if [ "$OLD_IP" == "$CURRENT_IP" ]; then
-    echo "IP address is unchanged. Update not required."
-else
-	echo "Updating dynv6 record with current public ip"
-	if [ "$RECORD_TYPE" == "A" ]; then
-		update=$(curl -sSL "http://ipv4.dynv6.com/api/update?zone=$ZONE&ipv4=$CURRENT_IP&token=$API_KEY")
-	else
-		update=$(curl -sSL "http://ipv6.dynv6.com/api/update?zone=$ZONE&ipv6=$CURRENT_IP&token=$API_KEY")
-	fi
-
-	if [ "$update" == "addresses updated" ]; then
-		echo "DNS Record $RECORD_NAME IP updated to $CURRENT_IP"
-		echo "$CURRENT_IP" > /old_record_ip
-	else
-		echo "Error updating dynv6 DNS record $RECORD_NAME"
-		echo "$update"
-	fi
+  echo "[$(date)]: IP unchanged, not updating. IP: $CURRENT_IP"
+	exit 0
 fi
 # #####################################################################
+# Step 3: Update ddns
+update=$(curl -sSL "http://${PROTO}.dynv6.com/api/update?zone=${ZONE}&${PROTO}=${CURRENT_IP}&token=${API_KEY}")
 
-print_breaker
+if [ "$update" == "addresses updated" ]; then
+	echo "[$(date)]: DDNS update successful...   IP: $CURRENT_IP"
+	echo "$CURRENT_IP" > /old_record_ip
+else
+	echo "[$(date)]: DDNS update failed...  Curr IP: $CURRENT_IP"
+	echo "$update"
+fi
+# #####################################################################
